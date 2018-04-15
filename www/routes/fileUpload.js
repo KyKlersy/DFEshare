31059e2a472dcf74e00c84e5fe44159f2ce7df34
Kyle <KyKlersy@gmail.com>
2018-04-15 15:13:02 -0500

    Updates

---
+++ www/routes/fileUpload.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var multer  = require('multer');
+var upload = multer({ dest: './www/uploads/' });
+
+var router = express.Router();
+
+router.post('/', upload.single('file') ,function(req, res, next) {
+  if (!req.file)
+  {
+    console.log("No file received");
+    return res.send({
+      success: false
+    });
+  } else
+  {
+    console.log(req.file + "File.");
+    console.log(Date.now());
+    res.send({
+      success: true;
+    });
+  }
+});
+
+module.exports = router;
---
+++ www/server.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const uploadFile = require(path.join(__dirname, '/routes/fileUpload'));
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, '/dist')));
+//app.use('/uploadfile', uploadFile);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+console.log(__dirname);
+
+app.listen(process.env.PORT || 3000);
